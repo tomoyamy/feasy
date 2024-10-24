4b71ce1e0f3a99b3118f1b067262503887c69591
solufa <solufa2020@gmail.com>
2024-10-25 01:23:01 +0900

    refactor; set PageExtensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx'],
   basePath:
     process.env.GITHUB_REPOSITORY !== undefined
       ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
