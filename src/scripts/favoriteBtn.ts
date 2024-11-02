fa127e0c7609536f5757e58686299b61cda461c2
tak42 <1206ookubo@gmail.com>
2024-11-02 10:21:46 +0900

    feat: favoriteBtn.ts

--- src/scripts/favoriteBtn.ts
+++ src/scripts/favoriteBtn.ts
@@ -1,1 +1,37 @@
-alert('Hello');
+type Style = {
+  height: string;
+  width: string;
+  color: string;
+  'background-color': string;
+};
+
+const createBtn = () => {
+  return document.createElement('button');
+};
+
+const alertFunc = () => {
+  alert('Hello World!!');
+};
+
+const btnStyle: Style = {
+  height: '50px',
+  width: '100px',
+  color: 'white',
+  'background-color': 'red',
+};
+
+const setBtnStyle = (btnElm: HTMLButtonElement, style: Style) => {
+  Object.keys(style).forEach((key) => {
+    btnElm.style.setProperty(key, style[key as keyof typeof style]);
+  });
+};
+
+const setBtnInnerText = (btnElm: HTMLButtonElement, txt: string) => {
+  btnElm.innerText = txt;
+};
+
+const alertBtn = createBtn();
+setBtnInnerText(alertBtn, 'アラート表示');
+setBtnStyle(alertBtn, btnStyle);
+alertBtn.addEventListener('click', alertFunc);
+document.body.appendChild(alertBtn);
